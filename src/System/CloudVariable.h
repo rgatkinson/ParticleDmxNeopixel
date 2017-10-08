036810d258a15ae4c4e156ce8abc4bf89fc28db0
Robert Atkinson <bob@theatkinsons.org>
2017-10-08 16:07:42 -0700

    simplify varaible announcing

--- src/System/CloudVariable.h
+++ src/System/CloudVariable.h
@@ -18,8 +18,6 @@ struct CloudVariable : SystemEventNotifications
 
     SETTING* _persistentSetting;
     String   _name;
-    bool     _begun = false;
-    bool     _connected = false;
 
     //----------------------------------------------------------------------------------------------
     // Construction
@@ -46,11 +44,6 @@ struct CloudVariable : SystemEventNotifications
     }
     void onCloudStatus(system_event_t event, int cloudStatus) override
     {
-        if (cloudStatus==cloud_status_connected)
-        {
-            _connected = true;
-            announce();
-        }
     }
     void onTimeChanged(system_event_t event, int timeStatus) override
     {
@@ -76,19 +69,15 @@ struct CloudVariable : SystemEventNotifications
 
     void announce()
     {
-        if (_begun && _connected)
+        INFO("announcing cloud variable name=%s value=%s", _name.c_str(), _persistentSetting->valueAsString().c_str());
+        bool success = Particle.variable(_name, this->valueRef());
+        if (success)
         {
-            INFO("announcing cloud variable name=%s value=%s", _name.c_str(), _persistentSetting->valueAsString().c_str());
-            bool success = Particle.variable(_name, this->valueRef());
-            if (success)
-            {
-                success = Particle.function(_name, static_cast<int (CloudVariable::*)(String)>(&CloudVariable::setValue), this);
-            }
-            if (!success)
-            {
-                ERROR("announcing cloud variable FAILED: %s", _name.c_str());
-            }
-
+            success = Particle.function(_name, static_cast<int (CloudVariable::*)(String)>(&CloudVariable::setValue), this);
+        }
+        if (!success)
+        {
+            ERROR("announcing cloud variable FAILED: %s", _name.c_str());
         }
     }
 
@@ -98,7 +87,6 @@ struct CloudVariable : SystemEventNotifications
 
     void begin()
     {
-        _begun = true;
         announce();
     }
 };
