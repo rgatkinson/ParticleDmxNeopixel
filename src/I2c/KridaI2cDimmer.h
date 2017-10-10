7042aed54bd90b07ff5e4f320ee516d74499a3b6
Robert Atkinson <bob@theatkinsons.org>
2017-10-10 09:34:50 -0700

    doc'n; better i2c address iniitiation

--- src/I2c/KridaI2cDimmer.h
+++ src/I2c/KridaI2cDimmer.h
@@ -9,6 +9,26 @@
 #include "I2c/KridaI2cParameterBlock.h"
 #include "Util/Deadline.h"
 
+// All the documentation we have :-( :
+//
+// KRIDA Electronics I2C AC Light Dimmer
+//
+// I2C Protocol -> first byte - device address
+//                 second byte - channel select
+//                 third byte - dimming value
+//
+// Device address -> 0x27, 0x26, 0x25, 0x24, 0x23, 0x22, 0x21, 0x20
+// Device address -> 0x3F, 0x3E, 0x3D, 0x3C, 0x3B, 0x3A, 0x39, 0x38
+//
+// Channels -> 0x80 1st channel
+//             0x81 2nd channel
+//             0x82 3rd channel
+//             0x83 4th channel
+//
+// Dimming value range 0...100
+//                     0 - Fully ON
+//                     100 - Fully OFF
+//
 struct KridaI2cDimmer : DmxPacketConsumer
 {
     //----------------------------------------------------------------------------------------------
@@ -52,6 +72,9 @@ protected:
     //----------------------------------------------------------------------------------------------
     // State
     //----------------------------------------------------------------------------------------------
+    static const int i2cAddressPCF8574T  = 0x20;   // '7 bit addr'
+    static const int i2cAddressPCF8574AT = 0x38;   // '7 bit addr'
+
 protected:
 
     static const DIMMER_VALUE _dimmerValueFirst = 0;
@@ -63,7 +86,7 @@ protected:
     static const int _msTransmissionInterval = 500;
 
     bool         _transmissionNeeded;
-    int          _i2cAddress = 0x3f;
+    int          _i2cAddress;
     ArtnetDevice _artnet;
     Deadline     _deadline;
     Dimmer       _dimmers[_channelCount];
@@ -73,11 +96,19 @@ protected:
     //----------------------------------------------------------------------------------------------
 public:
 
-    KridaI2cDimmer(LPCSTR shortName)
+    KridaI2cDimmer(int i2cAddressBase, int addressDipSwitch, LPCSTR shortName)
         : _artnet(this, DMX_ADDRESS_DEFAULT, shortName),
           _deadline(_msTransmissionInterval),
           _dimmers { {this,0}, {this,1}, {this,2}, {this,3} }
     {
+        Wire.setSpeed(CLOCK_SPEED_100KHZ);
+        Wire.begin();
+        _i2cAddress = i2cAddressBase + addressDipSwitch;
+    }
+
+    ~KridaI2cDimmer()
+    {
+        Wire.end();
     }
 
     //----------------------------------------------------------------------------------------------
